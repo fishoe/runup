e4216a5042946cefa9179c54fa40bf308a486ece
fishoe <m123oo@naver.com>
2020-12-12 20:07:57 +0900

    delete layout tmp in accounts, settings for ec2 s

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'postgres',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'admin',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432'
     }
 }
 
@@ -128,6 +135,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'runup_app/static'),
+    os.path.join(BASE_DIR, 'accounts/static'),
+]
 
 LOGIN_REDIRECT_URL = '/accounts/userinfo'
 LOGIN_URL = '/accounts/login'
